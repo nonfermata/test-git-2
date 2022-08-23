729e84623a18887018e1dd120b1970486c6f24ec
nonfermata <nonfermata@gmail.com>
2022-08-23 10:22:07 +0400

    well features

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('ok');
 
-const age = 20;
-if(age >= 18) {
+const someUserAge = 20;
+if(someUserAge >= 18) {
     console.log('Вы совершеннолетний.');
 } else {
     console.log('Вы НЕ совершеннолетний.');
